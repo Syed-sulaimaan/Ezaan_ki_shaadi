1b44be6e1d43b1ccd3e419db49325763a67fa1ec
sysu <sysulaimaan@gmail.com>
2024-08-05 11:42:37 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       
         {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none"> */}
           <a
-            className="fixed bottom-0 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            className="fixed bottom-6 pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
           >
             By{" His Brothers"}
             
